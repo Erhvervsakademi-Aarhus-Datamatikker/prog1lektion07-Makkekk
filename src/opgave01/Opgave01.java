724b957be4350c2d53828f85be11ae97456a4be8
Makkekk <karsten-kirkegaard@hotmail.com>
2024-09-13 11:12:53 +0200

    Opgave1 update

--- src/opgave01/Opgave01.java
+++ src/opgave01/Opgave01.java
@@ -1,12 +1,26 @@
 package opgave01;
 
+import java.sql.SQLOutput;
+
 public class Opgave01 {
     public static void main(String[] args) {
         int[] integerArray = getIntegerArray();
+
     }
 
-    private static int[] getIntegerArray() {
-        int[] integerArray =  { 1095, 12, 9065, 387, 700, 20, 5, 2065, 97654, 103, 789, 50, 1972, 200, 35, 98, 1002 };
+    private static int[] sum(int IntegerArrayGet) {
+        double total = 0;
+        for (int i = 0; i < IntegerArrayGet; i++) {
+            total += IntegerArrayGet[i];
+        }
+
+        private static int[] getIntegerArray (in) {
+        int[] integerArray = {1095, 12, 9065, 387, 700, 20, 5, 2065, 97654, 103, 789, 50, 1972, 200, 35, 98, 1002};
+
         return integerArray;
+        }
+
+
     }
+
 }
